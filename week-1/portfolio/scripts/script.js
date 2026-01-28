364aab201e3fd11bf5a2609ad16f2023570403a3
Ian Szymanski <iszymanski@ewu.edu>
2026-01-28 07:49:16 +0000

    add theme toggle functionality and related styles for improved user experience

---
+++ week-1/portfolio/scripts/script.js
@@ -0,0 +1,8 @@
+const toggle = document.querySelector(".theme-toggle");
+
+
+toggle.addEventListener("click", function () {
+    const currentTheme = document.documentElement.getAttribute("data-theme");
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
+    document.documentElement.setAttribute("data-theme", newTheme);
+});
