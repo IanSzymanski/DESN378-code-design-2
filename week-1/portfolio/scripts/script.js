a67db4a2bc103f65dec3c1ff8c6903df541e6f85
Ian Szymanski <iszymanski@ewu.edu>
2026-01-29 01:27:40 +0000

    Added local storage to data theme

--- week-1/portfolio/scripts/script.js
+++ week-1/portfolio/scripts/script.js
@@ -1,8 +1,15 @@
 const toggle = document.querySelector(".theme-toggle");
+const savedTheme = localStorage.getItem("theme") || "light";
 
+document.documentElement.setAttribute("data-theme", savedTheme);
+console.log("Current theme is:", savedTheme);
 
 toggle.addEventListener("click", function () {
     const currentTheme = document.documentElement.getAttribute("data-theme");
     const newTheme = currentTheme === "dark" ? "light" : "dark";
+
     document.documentElement.setAttribute("data-theme", newTheme);
+    console.log("Theme changed to:", newTheme);
+
+    localStorage.setItem("theme", newTheme);
 });
